ec6dc04df961d0d07614d5d9e45f4117bb329e72
Ribeirorafa02 <ribeirorafa02@icloud.com>
2026-09-13 21:07:06 -0300

    Fix formatting of static file serving path

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ async function ensureAdmin(){
 ensureAdmin();
 
 app.use(express.json({limit:"1mb"}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/health",(req,res)=>res.json({ok:true,name:"VerdeJá",version:"1.0.0"}));
 
